ce5eaf48a83953b10d9d25171f07a0549a7159d0
ishaaqj <info@ishaaq.net>
2025-02-17 17:28:44 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import styles from "./App.module.css";
+import { Navbar } from "./components/navbar/Navbar";
+
+function App() {
+return  (<div className={styles.App}>
+      <Navbar />
+      </div>
+);
+}
+
+export default App;
